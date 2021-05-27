aaea198170361f5acca387bd2b1f7815df556e88
TAN TYNN WEI <20019616@myrp.edu.sg>
2021-05-27 12:36:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/democontextmenutranslation2/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.democontextmenutranslation2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedText;
+
+    String wordClicked = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+        registerForContextMenu(tvTranslatedText);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0,0,0,"English");
+        menu.add(0,1,1,"Italian");
+
+        if(v == tvTranslatedText){
+            wordClicked = "hello";
+        }
+        else if(v == tvTranslatedText){
+            wordClicked = "bye";
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if(item.getItemId()==0) { //check whether the selected menu item ID is 0
+            //code for action
+            Toast.makeText(MainActivity.this, "English is chosen", Toast.LENGTH_SHORT).show();
+            tvTranslatedText.setText("Hello");
+
+            return true; //menu item successfully handled
+        }
+        else if(item.getItemId()==1) { //check if the selected menu item ID is 1
+            //code for action
+            Toast.makeText(MainActivity.this, "Italian is chosen", Toast.LENGTH_SHORT).show();
+            tvTranslatedText.setText("Ciao");
+
+            return true;  //menu item successfully handled
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+
+}
